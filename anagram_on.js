003c88652e3541c45aba075e064f642b43fdb2c3
Mateusz Przybyłowicz <uamfhq@gmail.com>
2019-01-16 14:33:31 +0100

    Enforce eslint rules

--- anagram_on.js
+++ anagram_on.js
@@ -2,42 +2,42 @@
 // Uses the frequency counter pattern.
 
 function validAnagram(first, second) {
-    // if the two words are not of the same length
-    // they're not anagrams
-    if (first.length !== second.length) {
-        return false;
-    }
+	// if the two words are not of the same length
+	// they're not anagrams
+	if (first.length !== second.length) {
+		return false;
+	}
 
-    // create an object to store letter frequency
-    const freq = {};
+	// create an object to store letter frequency
+	const freq = {};
 
-    // iterate through the word and set increment
-    // the letter frequency or set to 1 if not in the object
-    for (let letter of first) {
-        freq[letter] ? freq[letter] += 1 : freq[letter] = 1;
-    }
+	// iterate through the word and set increment
+	// the letter frequency or set to 1 if not in the object
+	for (let letter of first) {
+		freq[letter] ? freq[letter] += 1 : freq[letter] = 1;
+	}
 
-    // check the second word with the object
-    for (let letter of second) {
-        if (freq[letter]) {
-            freq[letter] -= 1;
-        } else {
-            return false;
-        }
-    }
+	// check the second word with the object
+	for (let letter of second) {
+		if (freq[letter]) {
+			freq[letter] -= 1;
+		} else {
+			return false;
+		}
+	}
 
-    // anagrams
-    return true;
+	// anagrams
+	return true;
 }
 
 // simple tests
 
-if (validAnagram("konstantynopolitańczykowianeczka", "konstantynopolitańczykowianeczka") 
-&& validAnagram("damn", "namd")
-&& validAnagram("rete", "tree")
-&& !validAnagram("konstantynopolitańczykowianeczka", "konstantynopolitańczykowianeczk") 
-&& !validAnagram("tree", "terr")) {
-    console.info("All tests OK!");
+if (validAnagram('konstantynopolitańczykowianeczka', 'konstantynopolitańczykowianeczka') 
+&& validAnagram('damn', 'namd')
+&& validAnagram('rete', 'tree')
+&& !validAnagram('konstantynopolitańczykowianeczka', 'konstantynopolitańczykowianeczk') 
+&& !validAnagram('tree', 'terr')) {
+	console.info('All tests OK!');
 } else {
-    console.warn("Tests failed!");
+	console.warn('Tests failed!');
 }
